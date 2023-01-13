ec4686551b65432200ec45fc27e7cb0ffeb58003
Tobi Olajide <tobiolajide887@yahoo.com>
2023-01-13 02:07:22 +0000

    added event listener to start timer on click of start button using function startQuiz

--- javaScript/logic.js
+++ javaScript/logic.js
@@ -1,24 +1,31 @@
 // Selects timer div element by id
 let timerEl = document.querySelector("#time");
-// set time for quiz to 100s
-let secondsLeft = 100;
+// Set time for quiz to 100s
+let secondsLeft = 10;
+// Selects start button element by id
+let startBtn = document.querySelector("#start")
 
-setTime();
-
-// function to set timer countdown
+// Function to set timer countdown
 function setTime() {
-  // Sets timer interval to 1s/1000ms
+  // sets timer interval to 1s/1000ms
   let timerInterval = setInterval(function() {
     // removes 1 from displayed time left for quiz
     secondsLeft--;
-    // set content of timer to time left
+    // sets content of timer to time left
     timerEl.textContent = secondsLeft;
-    
+
     if(secondsLeft === 0) {
-      // Stops execution of action when time runs out
+      // stops execution of action when time runs out
       clearInterval(timerInterval);
       console.log("Thanks for playing!")
     }
 
   }, 1000);
 }
+
+// function to handle start quiz click
+function startQuiz() {
+    setTime();
+}
+
+startBtn.addEventListener("click", startQuiz);
