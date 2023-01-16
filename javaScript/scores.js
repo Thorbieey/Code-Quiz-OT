7d93a1b755b64a14dd82f9b570a7780e52b4f46e
Tobi Olajide <tobiolajide887@yahoo.com>
2023-01-16 17:27:21 +0000

    select clear highscore button using querySelector

--- javaScript/scores.js
+++ javaScript/scores.js
@@ -1,10 +1,14 @@
 // select high scores ordered list element
 let highscoresEl = document.querySelector("#highscores");
+// select clear high scores button
+let clearHighscoreBtn = document.querySelector("#clear");
 
 // get stored highscore from localStorage
 let storedHighscore = JSON.parse(localStorage.getItem("highscore")); 
 
 renderHighscores();
+
+// Function to render highscores
 function renderHighscores() {
     for (let i = 0; i < storedHighscore[0].length; i++) {
         // create high score list element, and set their content 
