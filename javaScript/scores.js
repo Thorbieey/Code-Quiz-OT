3eb2650358b4f8e8fb943989e8178ab52ca57fe3
Tobi Olajide <tobiolajide887@yahoo.com>
2023-01-16 17:56:53 +0000

    added function to clear highscores using removeItem

--- javaScript/scores.js
+++ javaScript/scores.js
@@ -1,6 +1,6 @@
-// select high scores ordered list element
-let highscoresEl = document.querySelector("#highscores");
-// select clear high scores button
+// select highscores ordered list element
+let highscoresOl = document.querySelector("#highscores");
+// select clear highscores button
 let clearHighscoreBtn = document.querySelector("#clear");
 
 // get stored highscore from localStorage
@@ -10,10 +10,24 @@ renderHighscores();
 
 // Function to render highscores
 function renderHighscores() {
-    for (let i = 0; i < storedHighscore[0].length; i++) {
-        // create high score list element, and set their content 
-        let highscoreLi = document.createElement('li');
-        highscoreLi.textContent = storedHighscore[0][i] + " - " + storedHighscore[1][i];
-        highscoresEl.appendChild(highscoreLi);
+    highscoresOl.innerHTML = "";
+    if(storedHighscore !== null){
+        for (let i = 0; i < storedHighscore[0].length; i++) {
+            // create high score list element, and set their content 
+            let highscoreLi = document.createElement('li');
+            highscoreLi.textContent = storedHighscore[0][i] + " - " + storedHighscore[1][i];
+            highscoresOl.appendChild(highscoreLi);
+        }
     }
+    console.log("Highscores rendered");
 }
+
+// Function to clear highscores
+function clearHighscores() {
+    localStorage.removeItem('highscore');
+    storedHighscore = JSON.parse(localStorage.getItem("highscore")); 
+    console.log("Highscores cleared")
+    renderHighscores();
+}
+
+clearHighscoreBtn.addEventListener("click", clearHighscores)
